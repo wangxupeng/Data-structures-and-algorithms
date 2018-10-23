a71cd1d21c71dbe575afcd1b6c1d14c2e416365e
王旭鹏 <279354244@qq.com>
2018-10-23 11:12:17 +0800

    Create question_15.py

---
+++ Leetcode/question_15.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        n, res = len(nums), []
+        nums.sort()
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:   # 因为i=0这个元素会直接往下执行
+                continue
+            l, r = i+1, n-1
+            while l < r:
+                tmp = nums[i] + nums[l] + nums[r]
+                if tmp == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l-1]: 
+                        l += 1
+                    while l < r and nums[r] == nums[r+1]: 
+                        r -= 1
+                elif tmp > 0:
+                    r -= 1
+                else:
+                    l += 1
+        return res
