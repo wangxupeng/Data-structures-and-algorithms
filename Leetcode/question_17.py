cce0da75a45b3c075552898420489f262adf23ad
王旭鹏 <279354244@qq.com>
2018-10-23 12:22:20 +0800

    Update question_17.py

--- Leetcode/question_17.py
+++ Leetcode/question_17.py
@@ -4,26 +4,25 @@ class Solution:
         :type digits: str
         :rtype: List[str]
         """
-        if not digits:
+        if digits == "":
             return []
         
-        maps = {2:'abc', 
-                3:'def',
-                4:'ghi',
-                5:'jkl',
-                6:'mno',
-                7:'pqrs',
-                8:'tuv',
-                9:'wxyz'
-               }
-        
-        ret_lists = []
-        
-        def _traverse(cur_str):
-            if len(cur_str) == len(digits):
-                ret_lists.append(cur_str)
-                return
-            for char in maps[int(digits[char])]:
-                _traverse(cur_str + "{}".format(char))
-        _traverse("")
-        return ret_lists
+        d = {
+            2: "abc",
+            3: "def",
+            4: "ghi",
+            5: "jkl",
+            6: "mno",
+            7: "pqrs",
+            8: "tuv",
+            9: "wxyz"
+        }
+
+        cmbs = [""]
+        for num in digits:
+            new_combs = []
+            for cmb in cmbs:
+                for number in d[int(num)]:
+                    new_combs.append(cmb + number)
+            cmbs = new_combs
+        return cmbs
